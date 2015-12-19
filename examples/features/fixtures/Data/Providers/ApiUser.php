94ad08f7b37a83cb610615ead34274355e247b9d
Carlo Tasca <carlo.tasca.mail@gmail.com>
2015-12-19 11:00:12 +0000

    MGFX-03: Add ApiUser Data provider

---
+++ examples/features/fixtures/Data/Providers/ApiUser.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Data\Providers;
+
+use Magefix\Fixtures\Data\Provider;
+
+/**
+ * Class ApiUser
+ *
+ * @package Data\Providers
+ * @author  Carlo Tasca <ctasca@sessiondigital.com>
+ */
+class ApiUser implements Provider
+{
+
+}
