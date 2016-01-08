e51f635c2d9fab110f7b3178b61f3846b14e9045
ctasca <ctasca@sessiondigital.com>
2016-01-08 17:02:56 +0000

    MGFXR: Add getMap

--- magefix/src/Magefix/Magento/Model/Mapper.php
+++ magefix/src/Magefix/Magento/Model/Mapper.php
@@ -1,15 +1,45 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ctasca
- * Date: 08/01/2016
- * Time: 15:45
- */
 
 namespace Magefix\Magento\Model;
 
+use Magefix\Exceptions\UnmappableType;
+use Magefix\Fixture\Factory\Builder;
 
+/**
+ * Class Mapper
+ * @package Magefix\Magento\Model
+ */
 class Mapper
 {
+    /**
+     * @var array
+     */
+    private static $_map = [
+        Builder::SIMPLE_PRODUCT_FIXTURE_TYPE => 'catalog/product',
+        Builder::CONFIGURABLE_PRODUCT_FIXTURE_TYPE => 'catalog/product',
+        Builder::BUNDLE_PRODUCT_FIXTURE_TYPE => 'catalog/product',
+        Builder::CATEGORY_FIXTURE_TYPE => 'catalog/category',
+        Builder::CUSTOMER_FIXTURE_TYPE => 'customer/customer',
+        Builder::SALES_ORDER_FIXTURE_TYPE => 'sales/order',
+        Builder::API_USER_FIXTURE_TYPE => 'api/user'
+    ];
 
+    public static function getMap()
+    {
+        return self::$_map;
+    }
+
+    /**
+     * @param $fixtureType
+     * @return mixed
+     * @throws UnmappableType
+     */
+    public static function map($fixtureType)
+    {
+        if (!isset(self::$_map[$fixtureType])) {
+           throw new UnmappableType("Fixture type '{$fixtureType}' could not be mapped to any Magento model");
+        }
+
+        return self::$_map[$fixtureType];
+    }
 }
