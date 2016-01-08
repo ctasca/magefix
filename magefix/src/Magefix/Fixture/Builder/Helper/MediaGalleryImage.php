339fbe8f5caa2fc515f2e520f60bf84231e672bf
ctasca <ctasca@sessiondigital.com>
2016-01-08 17:28:07 +0000

    MGFXR: Modify helper

--- magefix/src/Magefix/Fixture/Builder/Helper/MediaGalleryImage.php
+++ magefix/src/Magefix/Fixture/Builder/Helper/MediaGalleryImage.php
@@ -1,15 +1,37 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ctasca
- * Date: 08/01/2016
- * Time: 17:17
- */
 
 namespace Magefix\Fixture\Builder\Helper;
 
+use Magefix\Exceptions\ProductMediaGalleryImageNotFound;
 
+/**
+ * Class MediaGalleryImage
+ * @package Magefix\Fixture\Builder\Helper
+ * @author Carlo Tasca <ctasca@sessiondigital.com>
+ */
 class MediaGalleryImage
 {
+    /**
+     * @param \Mage_Core_Model_Abstract $product
+     * @param array $gallery
+     * @param bool $save
+     * @throws ProductMediaGalleryImageNotFound
+     * @throws \Exception
+     */
+    public static function save(\Mage_Core_Model_Abstract $product, array $gallery, $save = true)
+    {
+        if (!@file_exists($gallery['image'])) {
+            throw new ProductMediaGalleryImageNotFound(
+                'Specified product fixture gallery image does not exists -> ' . $gallery['image']
+            );
+        }
 
+        $product->addImageToMediaGallery(
+            $gallery['image'], ['image', 'thumbnail', 'small_image'], false, false
+        );
+
+        if ($save) {
+            $product->save();
+        }
+    }
 }
